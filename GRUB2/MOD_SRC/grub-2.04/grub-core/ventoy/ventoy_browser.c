2bbafa9952af5c0c2b9e9089d179f78fe4e91542
Tnctr-altay-24 <Tnctr-altay-24@yandex.com>
2024-11-18 08:28:27 +0300

    Update ventoy_browser.c

--- GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_browser.c
+++ GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_browser.c
@@ -196,15 +196,15 @@ static int ventoy_browser_iterate_partition(struct grub_disk *disk, const grub_p
         browser_ssprintf(mbuf, "menuentry \"%s\" --class=vtoydisk {\n"
             "   echo \"unsupported file system type!\" \n"
             "   ventoy_pause\n"
-            "   set vtoy_iso_part=(hd0,1)
-            "   set ventoy_img_count=0\n"
-            "   vt_list_img vtoy_iso_part ventoy_img_count\n"
             "}\n",
             title);
     }
     else
     {
         browser_ssprintf(mbuf, "menuentry \"%s\" --class=vtoydisk {\n"
+            "   set vtoy_iso_part=(hd0,1)
+            "   set ventoy_img_count=0\n"
+            "   vt_list_img vtoy_iso_part ventoy_img_count\n"
             "  vt_browser_dir %s,%d 0x%lx /\n"
             "}\n",
             title, disk->name, partition->number + 1, (ulong)fs);
