6ebcc37c42c7e231a7ef0d12a480afc30324555f
Tnctr-altay-24 <Tnctr-altay-24@yandex.com>
2025-03-21 15:01:17 +0300

    Update ventoy_browser.c

--- GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_browser.c
+++ GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_browser.c
@@ -622,8 +622,7 @@ grub_err_t ventoy_cmd_browser_disk(grub_extcmd_context_t ctxt, int argc, char **
     (void)argc;
     (void)args;
 
-    browser_ssprintf(&mbuf, "source $prefix/FileManager.cfg", {\n
-                         "  echo 'return ...' \n}\n", "<--");
+    browser_ssprintf(&mbuf, "source $prefix/FileManager.cfg", "  echo 'return ...' \n", "<--");
 
     grub_disk_dev_iterate(ventoy_browser_iterate_disk, &mbuf);
 
