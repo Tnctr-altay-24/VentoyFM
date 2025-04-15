d38dfb3073409dc5357e68e1fda0d1c438fbc16f
Tnctr-altay-24 <Tnctr-altay-24@yandex.com>
2025-04-15 12:20:17 +0300

    Update ventoy_browser.c

--- GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_browser.c
+++ GRUB2/MOD_SRC/grub-2.04/grub-core/ventoy/ventoy_browser.c
@@ -146,7 +146,6 @@ static int ventoy_browser_iterate_partition(struct grub_disk *disk, const grub_p
     grub_fs_t fs;
     char *Label = NULL;
     browser_mbuf *mbuf = (browser_mbuf *)data;
-    grub_fs_t bs;
 
     (void)data;
 
@@ -170,13 +169,6 @@ static int ventoy_browser_iterate_partition(struct grub_disk *disk, const grub_p
         return 0;
     }
 
-    bs = grub_fs_probe(dev);
-    if (!bs)
-    {
-        grub_device_close(dev);
-        return 0;
-    }
-    
     fs->fs_label(dev, &Label);
 
     if (ventoy_check_file_exist("(%s)/.ventoyignore", partname))
